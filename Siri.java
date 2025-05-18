9591d09650ec148feabbfe84122b85d692e1286e
siri <malavathshirisha0@gmail.com>
2025-05-18 11:50:54 +0530

    updated

--- Siri.java
+++ Siri.java
@@ -6,6 +6,11 @@ public class Siri
         int num1=9;
         int sum =num+num1;
         System.out.println(sum);
+
+        int n1=4;
+        int n2=5;
+        int sum1=n1+n2;
+        System.out.println(sum1);
     }
 }
 
